cfc1c751c3604d7466c647d4fed2e72594c9b912
Bama94 <tegober94@hotmail.com>
2022-02-15 19:17:27 -0600

    moved mechanic and added money to societes

--- config.lua
+++ config.lua
@@ -6,5 +6,5 @@ Config.Jobs = {
     ['realestate'] = vector3(-124.786, -641.486, 168.820),
     ['taxi'] = vector3(903.32, -170.55, 74.0),
     ['cardealer'] = vector3(-32.0, -1114.2, 26.4),
-    ['mechanic'] = vector3(-347.41, -133.48, 39.01)
+    ['mechanic'] = vector3(-305.67, -121.01, 39.01)
 }
