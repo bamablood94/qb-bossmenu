6cd04570319eb33fd013b5ac5f6e47366515fe2f
Great-Liberty-RP <74937560+bamablood94@users.noreply.github.com>
2022-03-28 14:32:24 -0600

    ??

--- config.lua
+++ config.lua
@@ -7,5 +7,5 @@ Config.Jobs = {
     ['taxi'] = vector3(903.32, -170.55, 74.0),
     ['cardealer'] = vector3(-32.0, -1114.2, 26.4),
     ['mechanic'] = vector3(-305.67, -121.01, 39.01),
-    ['burgershot'] = 
+    ['snowsgunshow'] = vector3(12.54, -1105.8, 29.8)
 }
